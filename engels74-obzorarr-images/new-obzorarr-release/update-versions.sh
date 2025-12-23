75c4de77f19d8c43ea43ce19ebfcd951c56ddcd4
engels74 <141435164+engels74@users.noreply.github.com>
2025-12-23 22:23:57 +0100

    Remove temporary build directories [skip ci]

--- engels74-obzorarr-images/new-obzorarr-release/update-versions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/engels74/obzorarr/tags" | jq -re '.[0].name') || exit 1
-json=$(cat VERSION.json)
-jq --sort-keys \
-    --arg version "${version//v/}" \
-    '.version = $version' <<< "${json}" | tee VERSION.json
