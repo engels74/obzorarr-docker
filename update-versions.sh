102526e48248207d92831b61b562541bdf312bfb
engels74 <141435164+engels74@users.noreply.github.com>
2025-12-23 22:21:23 +0100

    Add nightly build configuration [skip ci]

---
+++ update-versions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/engels74/obzorarr/commits/main" | jq -re .sha) || exit 1
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    '.version = $version' <<< "${json}" | tee VERSION.json
